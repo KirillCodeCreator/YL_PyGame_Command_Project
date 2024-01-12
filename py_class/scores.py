bb9360a2608e2aeebb2f5b186831e6d2387c30e8
Kirill <okaydiscord@mail.ru>
2024-01-12 08:32:23 +0300

    поправил форматирование относительного пути к вайлам очков и констант

--- py_class/scores.py
+++ py_class/scores.py
@@ -5,14 +5,14 @@ class Scores:
 
     def save_to_file(self, scores):
         try:
-            with open('data\scores.txt', "w") as file:
+            with open('data/scores.txt', "w") as file:
                 file.write(str(scores))
         except FileNotFoundError:
             print('Файл scores.txt не найден')
 
     def get_scores_from_file(self):
         try:
-            with open('data\scores.txt', "r") as file:
+            with open('data/scores.txt', "r") as file:
                 result = '0'
                 for line in file:
                     result = line
